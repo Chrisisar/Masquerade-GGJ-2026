11e2b70c3d824f90edca0f7e7d267ab359422b7a
michal.hewelt <wicek3d@gmail.com>
2026-01-30 21:15:12 +0100

    Introduce Scoring page

--- Masquerade-Angular/Masquerade-Client/src/app/app.routes.ts
+++ Masquerade-Angular/Masquerade-Client/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LobbyComponent } from '../lobby/lobby.component';
 import { MaskCreatorComponent } from '../mask-creator/mask-creator.component';
 import { MaskComparisonComponent } from '../mask-comparison/mask-comparison.component';
+import { ScoringComponent } from '../mask-comparison/scoring/scoring.component';
 
 export const routes: Routes = [
   {
@@ -20,7 +21,10 @@ export const routes: Routes = [
   {
     path: 'mask-comparison',
     component: MaskComparisonComponent
+  },
+  {
+    path: 'scoring',
+    component: ScoringComponent
   }
-
   
 ];
