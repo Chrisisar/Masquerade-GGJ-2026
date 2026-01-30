9c1faed73070d6dd1da7da9dd362aa7484ab0f3e
michal.hewelt <wicek3d@gmail.com>
2026-01-30 19:45:25 +0100

    Introduce basic lobby component with routing

--- Masquerade-Angular/Masquerade-Client/src/app/app.routes.ts
+++ Masquerade-Angular/Masquerade-Client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LobbyComponent } from '../lobby/lobby.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'lobby',
+    component: LobbyComponent
+  },
+  {
+    path: '',
+    redirectTo: 'lobby',
+    pathMatch: 'full'
+  }
+];
